f4c45046a50a20de09b0337fda68b36ebece0936
ruchigoswami <awasthiruchi.us@gmail.com>
2017-10-23 18:40:38 -0400

    'initial'

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,27 @@
-// Inside the connection.js file, setup the code to connect Node to MySQL.
 
 var mysql = require('mysql');
+var connection;
 
-var connection ;
-if(process.env.JAWSDB_URL){ 
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
 
-connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'burgers_db'
 });
-};
+}
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+
+  console.log('connected as id ' + connection.threadId);
+
+});
+
 module.exports = connection;
-
