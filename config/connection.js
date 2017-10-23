a05dba73cddd4095c7c62427ef1fdf367be34cfa
ruchigoswami <awasthiruchi.us@gmail.com>
2017-10-23 18:35:19 -0400

    'initial'

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,19 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3306,
+var connection ;
+if(process.env.JAWSDB_URL){ 
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+
+connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'burgers_db'
 });
+};
 
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  };
-
-  console.log('connected as id ' + connection.threadId);
-
-});
-
+connection.connect();
 module.exports = connection;
 
